b611d34b787b545b48501e527726d78e5148cf90
Rémy Oudompheng <oudomphe@math.unice.fr>
2011-01-08 00:03:41 +0100

    Helper header.

---
+++ alpm-vala.h
@@ -0,0 +1,14 @@
+#include <alpm.h>
+
+pmpkg_t* alpm_pkg_load_file(const char *filename, int full)
+{
+    pmpkg_t *p;
+    int err = alpm_pkg_load(filename, full, &p);
+    if (err != 0) {
+        return NULL;
+    }
+    else {
+        return p;
+    }
+}
+
